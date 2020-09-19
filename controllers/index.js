df0794392c81095567d906d092400cb048bdc8a7
Manuel <manuel03ga11@hotmail.com>
2020-09-18 21:18:56 -0300

    Fixea bugs

--- controllers/index.js
+++ controllers/index.js
@@ -13,3 +13,15 @@ exports.home = (req, res) => {
 exports.search = (req, res) => {
   res.send('Implementar')
 }
+
+// controllers/index.js
+exports.search = (req, res) => {
+  let result = null;
+  User.findOne({ id: req.query.id }).exec((err, user) => {
+    if (user != null) {
+      user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
+      result = [user];
+    }
+    res.render('index', { users: result });
+  })
+}
